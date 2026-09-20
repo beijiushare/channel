35043b5dceaf2d7349abab41a461639acb54596d
beijiushare <beijiushare@outlook.com>
2026-09-21 01:07:27 +0800

    Update user links to use full Telegram URLs

--- src/main.ts
+++ src/main.ts
@@ -99,12 +99,12 @@ export default {
         replaceAttribute("src", "https:/", ""),
       )
       // 帖子头部的用户链接
-      .on('.tgme_widget_message_user a[href="/beijiutalk"]', 
+      .on('.tgme_widget_message_user a[href="https://t.me/beijiutalk"]', 
         replaceAttribute("href", "https://t.me", ""),
       )
 
       // 页面顶部的链接 
-      .on('a.tgme_header_link[href="/beijiutalk"]', 
+      .on('a.tgme_header_link[href="https://t.me/beijiutalk"]', 
         replaceAttribute("href", "https://t.me", ""),
       );
 
